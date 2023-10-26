7431498ceb0623d42b75c8052df8ec270933dd9b
Queengirl1990 <bruehlanja90@gmail.com>
2023-10-26 16:39:27 +0200

    "iconbar"

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'HomeScreen',
+    title: 'Bookie List',
     theme: ThemeData(
       primaryColor: Color(0xFFAC5859),
     ),
@@ -20,7 +20,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("HomeScreen"),
+        title: Text("HomeScreen"),
         backgroundColor: Color(0xFFAC5859),
       ),
       body: Center(
@@ -34,17 +34,61 @@ class HomeScreen extends StatelessWidget {
           child: Text("Gehe zu UnreadBooks"),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Unread Books',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.pushNamed(context, '/');
+          } else if (index == 1) {
+            Navigator.pushNamed(context, '/unreadBooks');
+          }
+        },
+      ),
     );
   }
 }
 
 class UnreadBooksScreen extends StatelessWidget {
-  final List<String> productNames = [
-    'Außerhalb der Schatten I',
-    'Vampires of Vensaya',
-    'Das Biest in ihm',
-    'Keep my silent heart',
-    'Ravenhall Academy I'
+  final List<Map<String, String>> books = [
+    {
+      'title': 'Außerhalb der Schatten',
+      'author': 'MJ Hard',
+      'year': '2020',
+      'image': 'https://bilder.buecher.de/produkte/61/61374/61374779n.jpg',
+    },
+    {
+      'title': 'Vampires of Vensaya',
+      'author': 'Michael Jeremy Hard',
+      'year': '2020',
+      'image': 'https://www.inforius-bilder.de/bild/?I=i9VAgp4zcKxSzWi5lnhuWtsPfe6L09SBfd%2Boa0pjO6A%3D',
+    },
+    {
+      'title': 'Das Biest in ihm',
+      'author': 'Serena Valentino',
+      'year': '2019',
+      'image': 'https://www.jugendbuch-couch.de/fileadmin/_processed_/7/4/csm_Das_Biest_c4b693978e.jpg',
+    },
+    {
+      'title': 'Keep my silent heart',
+      'author': 'Sazou G.',
+      'year': '2022',
+      'image': 'https://medien.umbreitkatalog.de/bildzentrale_original/978/375/653/2568.jpg',
+    },
+    {
+      'title': 'Ravenhall Academy I',
+      'author': 'Julia Kuhn',
+      'year': '2023',
+      'image': 'https://www.carlsen.de/sites/default/files/produkt/cover/verborgene-magie_4.jpg',
+    },
   ];
 
   @override
@@ -52,7 +96,7 @@ class UnreadBooksScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_sharp),
+          icon: Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -61,18 +105,46 @@ class UnreadBooksScreen extends StatelessWidget {
         backgroundColor: Color(0xFFAC5859),
       ),
       body: ListView(
-        children: productNames.map((productName) {
+        children: books.map((book) {
           return Card(
             child: ListTile(
               leading: Icon(Icons.book),
-              title: Text(productName),
+              title: Text(book['title']!),
+              subtitle: Text('${book['author']}, ${book['year']}'),
               onTap: () {
-                Navigator.pushNamed(context, '/bookDetails',
-                    arguments: 'Buchdetails $productName');
+                Navigator.pushNamed(
+                  context,
+                  '/bookDetails',
+                  arguments: {
+                    'title': book['title'],
+                    'author': book['author'],
+                    'year': book['year'],
+                    'image': book['image'],
+                  },
+                );
               },
             ),
           );
         }).toList(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Unread Books',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.pushNamed(context, '/');
+          } else if (index == 1) {
+            Navigator.pushNamed(context, '/unreadBooks');
+          }
+        },
       ),
     );
   }
@@ -81,28 +153,16 @@ class UnreadBooksScreen extends StatelessWidget {
 class BookDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    String info = ModalRoute.of(context)!.settings.arguments as String;
-    String bookTitle = info.replaceAll('Buchdetails ', '');
-    String imagePath = '';
+    Map<String, dynamic> bookInfo = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    String title = bookInfo['title'];
+    String author = bookInfo['author'];
+    String year = bookInfo['year'];
+    String image = bookInfo['image'];
 
-    if (bookTitle == 'Außerhalb der Schatten I') {
-      imagePath = 'https://bilder.buecher.de/produkte/61/61374/61374779n.jpg';
-    } else if (bookTitle == 'Vampires of Vensaya') {
-      imagePath =
-          'https://www.inforius-bilder.de/bild/?I=i9VAgp4zcKxSzWi5lnhuWtsPfe6L09SBfd%2Boa0pjO6A%3D';
-    } else if (bookTitle == 'Das Biest in ihm') {
-      imagePath =
-          'https://www.jugendbuch-couch.de/fileadmin/_processed_/7/4/csm_Das_Biest_c4b693978e.jpg';
-    } else if (bookTitle == 'Keep my silent heart') {
-      imagePath =
-          'https://medien.umbreitkatalog.de/bildzentrale_original/978/375/653/2568.jpg';
-    } else if (bookTitle == 'Ravenhall Academy I') {
-      imagePath =
-          'https://www.carlsen.de/sites/default/files/produkt/cover/verborgene-magie_4.jpg';}
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_sharp),
+          icon: Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -114,26 +174,52 @@ class BookDetailsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (imagePath.isNotEmpty)
-              Image.network(
-                imagePath,
-                width: 100, // Ändere die Breite nach Bedarf
-                height: 150, // Ändere die Höhe nach Bedarf
+            if (image.isNotEmpty)
+              Stack(
+                alignment: Alignment.topLeft,
+                children: [
+                  Image.network(
+                    image,
+                    width: 100,
+                    height: 150,
+                  ),
+                ],
               ),
-            Text(info),
+            Text('Buchdetails:'),
+            Text('Titel: $title'),
+            Text('Autor: $author'),
+            Text('Erscheinungsjahr: $year'),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/'); // Navigiere zum HomeScreen
               },
               style: ElevatedButton.styleFrom(
-                primary: Color(0xFFAC5859),
+                backgroundColor: Color(0xFFAC5859),
               ),
               child: Text("Zurück zum HomeScreen"),
             ),
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Unread Books',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.pushNamed(context, '/');
+          } else if (index == 1) {
+            Navigator.pushNamed(context, '/unreadBooks');
+          }
+        },
+      ),
     );
   }
 }
-
