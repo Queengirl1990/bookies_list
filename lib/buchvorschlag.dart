6fe9f0bbea99250c263775d78ee10e75c5f2b82b
Queengirl1990 <bruehlanja90@gmail.com>
2023-11-08 19:32:36 +0100

    datenbank ausbau

--- lib/buchvorschlag.dart
+++ lib/buchvorschlag.dart
@@ -48,41 +48,41 @@ class _BookSuggestionScreenState extends State<BookSuggestionScreen> {
         ),
       ),
       backgroundColor: darkRed,
-      body: ListView.builder(
-        itemCount: bookSuggestion.length,
-        itemBuilder: (context, index) {
-          final bookData = bookSuggestion.values.toList()[index];
-          final bookTitle = bookData['title']!;
-          final bookAuthor = bookData['author']!;
-          final bookGenre = bookData['genre']!;
-          final bookYear = bookData['year']!;
-          final bookImage = bookData['image']!;
-          final bookBlurb = bookData['blurb']!;
+      body: Center(
+        child: Container(
+          width: 320, // Breite des Containers auf 320 Pixel begrenzen
+          child: ListView.builder(
+            itemCount: bookSuggestion.length,
+            itemBuilder: (context, index) {
+              final bookData = bookSuggestion[index];
+              final bookTitle = bookData.title;
+              final bookAuthor = bookData.author;
+              final bookGenre = bookData.genre;
+              final bookYear = bookData.year;
+              final bookImage = bookData.image;
+              final bookBlurb = bookData.blurb;
 
-          return Container(
-            width: 320,
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                const SizedBox(
-                  width: 320,
-                  child: Divider(
-                    color: snowWhite,
-                    height: 20,
-                    thickness: 2,
+              return Column(
+                children: [
+                  const SizedBox(
+                    width: 320,
+                    child: Divider(
+                      color: Colors.white,
+                      height: 20,
+                      thickness: 2,
+                    ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.favorite, size: 20, color: Colors.grey),
-                    SizedBox(width: 10),
-                    Icon(Icons.favorite, size: 20, color: Colors.grey),
-                    SizedBox(width: 10),
-                    Icon(Icons.favorite, size: 20, color: Colors.grey),
-                  ],
-                ),
-                const SizedBox( 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.favorite, size: 20, color: lightGrey),
+                      SizedBox(width: 10),
+                      Icon(Icons.favorite, size: 20, color: lightGrey),
+                      SizedBox(width: 10),
+                      Icon(Icons.favorite, size: 20, color: lightGrey),
+                    ],
+                  ),
+                  const SizedBox( 
                 width: 320,
                 child: const Divider( 
                   color: snowWhite,
@@ -90,75 +90,76 @@ class _BookSuggestionScreenState extends State<BookSuggestionScreen> {
                   thickness: 2,
                 ),
               ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.asset(bookImage, width: 100, height: 150),
-                    ),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            bookTitle,
-                            style: TextStyle(
-                              fontFamily: 'DancingScript',
-                              fontWeight: FontWeight.normal,
-                              fontSize: 18,
-                              color: Colors.white,
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(bookImage, width: 100, height: 150),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              bookTitle,
+                              style: TextStyle(
+                                fontFamily: 'DancingScript',
+                                fontWeight: FontWeight.normal,
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          Text(
-                            'Author: $bookAuthor',
-                            style: TextStyle(
-                              color: Colors.white,
+                            Text(
+                              'Author: $bookAuthor',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          Text(
-                            'Genre: $bookGenre',
-                            style: TextStyle(
-                              color: Colors.white,
+                            Text(
+                              'Genre: $bookGenre',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          Text(
-                            'Year: $bookYear',
-                            style: TextStyle(
-                              color: Colors.white,
+                            Text(
+                              'Year: $bookYear',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      bookBlurb,
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
                     ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    bookBlurb,
-                    style: TextStyle(
-                      color: Colors.white,
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Hier kannst du die Interaktion hinzufügen
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.black,
+                      elevation: 5,
                     ),
+                    child: Text("Auswählen"),
                   ),
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    // Hier kannst du die Interaktion hinzufügen
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.black,
-                    elevation: 5,
-                  ),
-                  child: Text("Auswählen"),
-                ),
-              ],
-            ),
-          );
-        },
+                ],
+              );
+            },
+          ),
+        ),
       ),
     );
   }
