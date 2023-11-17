017a4b78e99ed9b1a7f185d5c74c95366da7c025
Queengirl1990 <bruehlanja90@gmail.com>
2023-11-17 20:17:23 +0100

    entfernen scanner

--- lib/screens/neuesbuchscannen.dart
+++ lib/screens/neuesbuchscannen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../styles.dart';
 import 'package:bookieslist/widgets/bookieslist-widgets.dart';
-import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,25 +9,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: NewBookScann(),
     );
   }
 }
 
 class NewBookScann extends StatefulWidget {
-  const NewBookScann({Key? key}) : super(key: key);
+  const NewBookScann({super.key});
 
   @override
   _NewBookScannState createState() => _NewBookScannState();
 }
 
 class _NewBookScannState extends State<NewBookScann> {
-  bool isScanning = false;
-  String scannedResult = '';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +38,7 @@ class _NewBookScannState extends State<NewBookScann> {
           },
         ),
         actions: [
-          myCircularAvatar(),
+          myCircularAvatar(), // myCircularAvatar bleibt oben rechts
         ],
       ),
       backgroundColor: darkRed,
@@ -63,75 +59,12 @@ class _NewBookScannState extends State<NewBookScann> {
           Center(
             child: Image.asset(
               'assets/images/scanner-logo.png',
-              width: 100,
-              height: 100,
+              width: 100, // Passen Sie die Breite nach Bedarf an
+              height: 100, // Passen Sie die Höhe nach Bedarf an
             ),
           ),
           const MyDividerWithIcons(),
-          ElevatedButton(
-            onPressed: () async {
-              var res = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SimpleBarcodeScannerPage(),
-                ),
-              );
-              setState(() {
-                if (res is String) {
-                  scannedResult = res;
-                }
-              });
-            },
-            child: const Text('Open Scanner'),
-          ),
-          Text('Scanned Result: $scannedResult'),
         ],
-      ),
-    );
-  }
-}
-
-class SimpleBarcodeScannerPage extends StatefulWidget {
-  const SimpleBarcodeScannerPage({Key? key}) : super(key: key);
-
-  @override
-  _SimpleBarcodeScannerPageState createState() =>
-      _SimpleBarcodeScannerPageState();
-}
-
-class _SimpleBarcodeScannerPageState
-    extends State<SimpleBarcodeScannerPage> {
-  String result = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Barcode Scanner'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                var res = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SimpleBarcodeScannerPage(),
-                  ),
-                );
-                setState(() {
-                  if (res is String) {
-                    result = res;
-                  }
-                });
-              },
-              child: const Text('Open Scanner'),
-            ),
-            Text('Barcode Result: $result'),
-          ],
-        ),
       ),
     );
   }
