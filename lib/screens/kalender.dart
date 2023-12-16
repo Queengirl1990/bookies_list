62616ac76273133e28bf2bd99035e26acd587bac
Queengirl1990 <bruehlanja90@gmail.com>
2023-12-16 22:06:50 +0100

    kalender rein optisch

--- lib/screens/kalender.dart
+++ lib/screens/kalender.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../styles/farbcodes.dart';
 import 'package:bookieslist/styles/appbar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,10 +38,31 @@ class _CalenderScreenState extends State<CalenderScreen> {
   }
 }
 
-//Screen bauen
+//Kalender
 class YourBodyWidget extends StatelessWidget {
+  List<dynamic> _eventsForDay(DateTime day) {
+    // Beispiel
+    if (day.isAtSameMomentAs(DateTime.utc(2023, 1, 1))) {
+      return ['Event 1', 'Event 2'];
+    }
+
+    if (day.isAtSameMomentAs(DateTime.utc(2023, 1, 2))) {
+      return ['Event 3', 'Event 4'];
+    }
+
+    return [];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final DateTime now = DateTime.now();
+    final DateTime today = DateTime(now.year, now.month, now.day);
+
+    return TableCalendar(
+      firstDay: DateTime.utc(2023, 1, 1),
+      lastDay: DateTime.utc(2023, 12, 31),
+      focusedDay: today,
+      eventLoader: _eventsForDay,
+    );
   }
 }
