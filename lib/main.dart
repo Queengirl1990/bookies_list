7d366249605ed751107acf0fb174d2d444dbd4b0
Queengirl1990 <bruehlanja90@gmail.com>
2023-12-21 15:43:57 +0100

    updatet splash

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:bookieslist/home/home.dart';
 import 'package:bookieslist/unreadBooks/sub.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  print('App gestartet');
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
